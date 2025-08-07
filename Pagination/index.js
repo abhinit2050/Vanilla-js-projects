f32f62b3b96da42c6370f6e85b0973559962194a
Abhinit Rai <abhinit1990@gmail.com>
2025-08-07 21:32:57 +0530

    pagination

--- Pagination/index.js
+++ Pagination/index.js
@@ -1,4 +1,4 @@
-const allItems = new Array(200).fill().map((_,index)=> `This is line number ${index+1}`);
+const allItems = new Array(100).fill().map((_,index)=> `This is line number ${index+1}`);
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 const pageNoDisplay = document.querySelector('#pageNoDisplay');
@@ -12,13 +12,58 @@ const itemStyle = {
     border:'1px solid black'
 }
 const parentContainer = document.querySelector('.paginationContainer');
-const itemsPerPage = 10;
-const totalPages = Math.ceil(allItems.length/itemsPerPage)
+const itemsPerPage = 20;
+const totalPages = Math.ceil(allItems.length/itemsPerPage);
 
-allItems.forEach((item, index)=>{
+function renderListGenerator(){
+     pageNoDisplay.value = currentPageNumber;
+    let startIndex = (currentPageNumber-1)*itemsPerPage
+    let renderList = allItems.slice(startIndex, startIndex+itemsPerPage);
+    main(renderList);
+}
+
+
+function main(arr){
+    if(currentPageNumber >= 1){
+        parentContainer.replaceChildren();
+    }
+
+    if(currentPageNumber<=1){
+        prevBtn.disabled = true;
+    }
+
+    if(currentPageNumber >= totalPages){
+        nextBtn.disabled = true;
+    }
+
+    if(currentPageNumber>1 && currentPageNumber < totalPages){
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
+
+    arr.forEach((item, index)=>{
     if(index >= itemsPerPage) return; // Only render the first page for now
     const rowElement = document.createElement('div');
     rowElement.textContent = item;
     Object.assign(rowElement.style, itemStyle);
     parentContainer.appendChild(rowElement);
 })
+}
+
+nextBtn.addEventListener("click",()=>{
+    currentPageNumber += 1;
+    renderListGenerator();
+})
+
+prevBtn.addEventListener("click",()=>{
+    if(currentPageNumber <1) return
+    currentPageNumber -= 1;
+    renderListGenerator();
+})
+
+document.addEventListener("DOMContentLoaded",()=>{
+    currentPageNumber = 1;
+    renderListGenerator();
+})
+
+console.log('total', totalPages);
